f235a804083c064eaa4dc168bdbc03185feba550
Alerran Victor <alerransilva13@gmail.com>
2022-11-22 20:13:48 -0300

    rec-SA2

---
+++ SA2/rec-SA2/script.js
@@ -0,0 +1,7 @@
+let email = document.getElementById("email").value
+let senha = document.getElementById("senha").value
+
+function enviarEmail(event) {
+    console.log(email)
+    console.log(senha)
+}
